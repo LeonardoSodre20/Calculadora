9e04c2d3de9a6ff5b395eb1959cad9e0bb4cd866
Leonardo Sodré <LeonardoSodre25@outlook.com>
2022-02-01 21:44:34 -0300

    em andamento

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+class Calculator{
+    constructor(){
+        
+    }
+}
+
